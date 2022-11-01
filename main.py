cd4f4a0b3f0429899678bf5b713e7a1b43347f0e
whale <whalebox4.5@gmail.com>
2022-11-02 03:52:43 +0800

    jinja template

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
-from flask import Flask, render_template
+import sqlite3
+from unittest import result
+from flask import Flask, render_template, g, current_app
+from blogpost import blogpost_bp
+import os
 
 app = Flask(__name__)
 
+@app.cli.command("init-db")
+def init_db():
+    db = sqlite3.connect("instance/posts.sqlite")
+    cur = db.cursor()
+    with open('schema.sql','r', encoding='utf-8') as f:
+        cur.executescript(f.read())
+    
 @app.route("/")
 def index():
-    return render_template('index.html')
+    db = sqlite3.connect("instance/posts.sqlite")
+    result = db.execute("""
+        SELECT title, created, body, id FROM post
+        ORDER BY created DESC LIMIT 5
+    """)
+    rlist = result.fetchall()
+    return render_template('index.html',result = rlist)
 
 @app.route("/blogpost")
 def blogpost():
-    return render_template('blog-post.html')
+    return render_template('blog-post-template.html')
 
 @app.route("/bloglist")
 def bloglist():
-    return render_template('blog-list.html')
+    db = sqlite3.connect("instance/posts.sqlite")
+    result = db.execute("""
+        SELECT title, created, body, id FROM post
+        ORDER BY created DESC
+    """)
+    rlist = result.fetchall()
+    return render_template('blog-list.html', result = rlist)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+app.register_blueprint(blogpost_bp)
